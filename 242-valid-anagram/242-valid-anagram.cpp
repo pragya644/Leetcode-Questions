3b1d861704f719312009267cf33e279de79537ba
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-01-10 11:25:11 +0530

    Time: 10 ms (57.95%), Space: 7.3 MB (44.55%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s==t)
-            return true;
-        return false;
+        if(s.length()!=t.length())
+            return false;
+        int count[26] = {0};
+        for(int i=0; i<s.length(); i++)
+            count[s[i]-'a']++;
+        for(int i=0; i<t.length(); i++)
+            count[t[i]-'a']--;
+        for(int i=0; i<26; i++)
+            if(count[i]!=0)
+                return false;
+        return true;
     }
 };
