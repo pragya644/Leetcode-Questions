bc45eabd52a5861473a66e0caf257ecd8071fb14
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-01-27 14:09:07 +0530

    Time: 65 ms (22.79%), Space: 13.1 MB (76.43%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
+    int lowest_count(vector<vector<int>> &matrix,int mid)
+    {
         int n = matrix.size();
-        int i = 0;
-        k = (n*n)-k+1;
-        while(i<n)
+        int count = 0;
+        
+        for(int i=0; i<n; i++)
         {
-            int j = 0;
-            while(j<n)
+            for(int j=n-1; j>=0; j--)
             {
-                pq.push(matrix[i][j]);
-                j++;
-                if(pq.size()>k)
-                    pq.pop();
+                if(matrix[i][j]<=mid){
+                    count += j+1; 
+                     break;
+                }
             }
-            i++;
         }
-        return pq.top();
+        return count;
+    }
+    
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        if(n==1)
+            return matrix[n-1][n-1];
+        int ans = 0;
+        int l = matrix[0][0];
+        int h = matrix[n-1][n-1];
+        while(l<h)
+        {
+           int mid = ((h-l)/2)+l;
+            int count = lowest_count(matrix,mid);
+            if(count>=k)
+                h = mid;
+            else
+                l = mid+1;
+        }
+        return l;
     }
 };
