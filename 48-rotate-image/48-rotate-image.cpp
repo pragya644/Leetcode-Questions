b4101b21a4ea4215485b09b9f15ce48a4f2a60a3
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-02-02 13:46:19 +0530

    Time: 3 ms (62.89%), Space: 7.2 MB (33.49%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    void transpose(vector<vector<int>> &m)
+    void transpose(vector<vector<int>> &mat)
     {
-        int n = m.size();
+        int n = mat.size();
         for(int i=0; i<n; i++)
         {
-            for(int j= i+1; j<n; j++)
-            {
-                if(i!=j)
-                    swap(m[i][j], m[j][i]);
-            }
+            for(int j=i+1; j<n; j++)
+                swap(mat[i][j],mat[j][i]);
         }
     }
     
-    void rotate(vector<vector<int>>& m) {
-        transpose(m);
-        int n = m.size();
+    void rotate(vector<vector<int>>& mat) {
+        transpose(mat);
+        int n = mat.size();
         int left = 0;
         int right = n-1;
         while(left<right)
         {
             for(int i=0; i<n; i++)
-            {
-                swap(m[i][left],m[i][right]);
-            }
+                swap(mat[i][left],mat[i][right]);
             left++;
             right--;
         }
