08ac543de1e2fa50e8de3328093139be76ba52c7
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-02-03 15:29:49 +0530

    Time: 5 ms (12.36%), Space: 5.8 MB (98.51%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int memo[101][101];
-    int solve(int m,int n,int x,int y)
+    
+    int solve(int x,int y,int m,int n)
     {
-        if(memo[m][n]!=-1)
-            return memo[m][n];
-        if(m>=x ||n>=y)
-            return memo[m][n] = 0;
-        if(m==x-1 || n==y-1)
-           return  memo[m][n]= 1;
-        return memo[m][n] = solve(m+1,n,x,y)+solve(m,n+1,x,y);
+        if(memo[x][y]!=-1)
+            return memo[x][y];
+        if(x>=m || y>=n)
+            return memo[x][y]==0;
+        if(x==m-1 || y==n-1)
+            return memo[x][y] = 1;
+        return memo[x][y] = solve(x+1,y,m,n) + solve(x,y+1,m,n);
     }
     
     int uniquePaths(int m, int n) {
-        int x = m;
-        int y = n;
-        memset(memo,-1,sizeof(memo));
-        return solve(0,0,x,y);
+        memset(memo,-1, sizeof(memo));
+        int ans = solve(0,0,m,n);
+        return ans;
     }
 };
