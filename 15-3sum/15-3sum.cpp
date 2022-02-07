7776e9ecec9a4540bcc3461f3cae80a73d838dea
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-02-07 19:32:22 +0530

    Time: 60 ms (95.01%), Space: 20 MB (66.77%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        if(nums.empty())
+        int n = nums.size();
+        if(n<3 || n==0)
             return {};
         vector<vector<int>> res;
-        int n = nums.size();
-        if(n<3)
-            return {};
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         for(int i=0; i<n-2; i++)
         {
+            // -1 -1 0 1 2 4 4
+            //  f  l         r
+            //  0-f = 0-(-1) = 1;
+            //  l+r = -1 + 4 = 3;  half_sum>left_sum
+            //       l   r 
             if(i>0 && nums[i]==nums[i-1])
                 continue;
-            int a = nums[i], l= i+1, r= n-1;
-            while(l<r)
+            int left = i+1;
+            int right = n-1;
+            int first = nums[i];
+            while(left<right)
             {
-                int diff = nums[l] + nums[r];
-                int sum = 0 - a;
-                if(diff== sum)
+                // -2 + 3 -1 = 0
+                // -2 + 3 = 0 - (-1);
+                //  l + r =  0 -  f
+                
+                int half_sum = nums[left] + nums[right];
+                int left_sum = 0 - first;
+                if(half_sum == left_sum)
                 {
-                    res.push_back({a,nums[l],nums[r]});
-                    int left = nums[l];
-                    int right = nums[r];
-                    while(l<r && left==nums[l])
-                    {
-                        l++;
-                    }
-                    while(l<r && right==nums[r])
-                    {
-                        r--;
-                    }
+                    res.push_back({first, nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while( left<right && nums[left]==nums[left-1])
+                        left++;
+                    while( left<right && nums[right]==nums[right+1])
+                        right--;
                 }
-                else if(diff < sum)
-                    l++;
+                else if(half_sum< left_sum)
+                    left++;
                 else
-                    r--;
+                    right--;
             }
         }
         return res;
