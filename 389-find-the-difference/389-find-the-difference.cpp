e23094a195fa67633a49959a89700fbfc6911f84
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-02-07 13:57:34 +0530

    Time: 4 ms (67.41%), Space: 6.8 MB (25.35%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int lens = s.length()-1;
-        int lent = t.length()-1;
-        while(lens>=0 && lent>=0)
-        {
-            if(s[lens]!=t[lent])
-                return t[lent];
-            lens--;
-            lent--;
-        }
-        return t[0];
+        char r = 0;
+        for(char c: s)
+            r = r^c;
+        for(char c: t)
+            r = r^c;
+        return r;
     }
 };
