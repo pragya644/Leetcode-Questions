ae444b41fa1f136f71c69727b881a5c6557bc633
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-02-13 16:46:16 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (85.44%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void rec(vector<int> &nums, vector<int> &temp,int i,vector<vector<int>>&ans)
+    void solve(vector<int> &nums, vector<int> &temp, int i, vector<vector<int>> &res)
     {
         if(i==nums.size())
-            ans.push_back(temp);
-        else
         {
-            temp.push_back(nums[i]);
-            rec(nums,temp,i+1,ans);
-            temp.pop_back();
-            rec(nums,temp,i+1,ans);
+            res.push_back(temp);
+            return;
         }
+        temp.push_back(nums[i]);
+        solve(nums, temp, i+1, res);
+        temp.pop_back();
+        solve(nums, temp, i+1, res );
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
         vector<vector<int>> res;
-        rec(nums,temp,0,res);
+        solve(nums,temp,0,res);
         return res;
     }
 };
