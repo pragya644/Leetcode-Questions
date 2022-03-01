7fd8620a781db75a14f1d8b196a734eb0c9d0e7d
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-03-01 11:14:47 +0530

    Time: 6 ms (65.69%), Space: 7.8 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    int bitscount(int n)
-    {
-        int temp = n;
-        int count = 0;
-        while(temp>0)
-        {
-            if(temp%2==1)
-                count++;
-            temp = temp/2;
-        }
-        return count;
-    }
-    
     vector<int> countBits(int n) {
-        vector<int> ans;
+        vector<int> ans(n+1);
+        ans[0] = 0;
         for(int i=0; i<=n; i++)
         {
-            ans.push_back(bitscount(i));
+            ans[i] = ans[i/2] + i%2;
         }
         return ans;
     }
