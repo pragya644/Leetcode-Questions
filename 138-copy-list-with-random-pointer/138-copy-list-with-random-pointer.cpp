4d88a1da0f5cc3fdfac0184b5062c80543692e18
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-03-13 00:39:01 +0530

    Time: 15 ms (40.72%), Space: 11.3 MB (27.27%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-    
+                                                            
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -13,6 +13,26 @@ public:
     }
 };
 */
+
+
+                                                                      // 7 3 2 1 Null
+//                                                                    // 7 3 2 1
+//                                                      1st iteration  7->7->3->3->2->2->1->1->null
+//                                                                    0  c  0  c  0  c  0  c
+//                                                                    3->random = 7
+//                                                     2nd iteration  3->next->random = 3->random->next
+//                                                                    3(o)->3(c)->random = 3(o)->7(o)->7(c);
+//                                                     3rd iteraton   1 (ans node)
+//                                                                    1 (helper node)
+//                                                                    helper->next = head->next
+//                                                                    1->next = 7(o)->7(c)   
+//                                                                    1->next = 7(c)
+//                                                                    head->next = head->next->next
+//                                                                    7(o)->next = 7(o)->7(c)->3(o)
+//                                                                    head = head->next
+//                                                                   head = 7(o)->3;
+                                                                   
+//                                                     return         ans->next;
 
 class Solution {
 public:
