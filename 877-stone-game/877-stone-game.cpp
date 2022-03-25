60ed7123fe36bc3d5ab019c4a861a7a667364aa6
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-03-25 19:57:13 +0530

    Time: 16 ms (44.74%), Space: 8.8 MB (35.08%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -2,18 +2,29 @@ class Solution {
 public:
     int dp[501][501];
     
-    int solve(int i, int j, vector<int> &p)
+    int solve(int i, int j, bool turn, vector<int>& piles)
     {
         if(i>j)
             return 0;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        return dp[i][j] = max(p[i]-solve(i+1,j,p), p[j]-solve(i,j-1, p));
+        if(turn)
+        {
+            int calc1 = solve(i+1,j,false,piles)+piles[i];
+            int calc2 = solve(i, j-1, false, piles)+piles[j];
+            dp[i][j] = max(calc1,calc2);
+            return dp[i][j];
+        }
+        int calc1 = solve(i+1,j,true,piles)-piles[i];
+        int calc2 = solve(i,j-1,true,piles)-piles[j];
+        dp[i][j] = min(calc1,calc2);
+        return dp[i][j];
     }
     
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        memset(dp,-1, sizeof(dp));
-        return solve(0,n-1,piles)>0;
+        bool turn = true;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,n-1,turn,piles)>0;
     }
 };
