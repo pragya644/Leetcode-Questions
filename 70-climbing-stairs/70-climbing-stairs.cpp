54502f2734ce0d0a4a12d83a5957fc87858fab7c
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-03-30 00:15:47 +0530

    Time: 4 ms (21.42%), Space: 6 MB (55.45%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int t[n+1];
-        t[0] = 1,t[1]=1;
+        if(n<=1)
+            return 1;
+        int dp[n+1];
+        dp[0] = dp[1] = 1;
         for(int i=2; i<=n; i++)
-        {
-            t[i] = t[i-1] + t[i-2];
-        }
-        return t[n];
+            dp[i] = dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
