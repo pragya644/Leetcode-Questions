e89b259f504798ddef96735398d70ad5f1927e2d
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-03-29 23:47:17 +0530

    Time: 6 ms (46.75%), Space: 8.8 MB (50.69%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -6,15 +6,19 @@ public:
             return 0;
         if(nums[0]>nums[1])
             return 0;
-        for(int i=1; i<n-1; i++)
-        {
-            if(i+1<n && nums[i]>nums[i+1] && nums[i]>nums[i-1])
-            {
-                return i;
-            }
-        }
         if(nums[n-1]>nums[n-2])
             return n-1;
+        int l=1, r = n-2;
+        while(l<=r)
+        {
+            int mid = l+(r-l)/2;
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
+                return  mid;
+            else if(nums[mid]<nums[mid-1])
+                r = mid-1;
+            else if(nums[mid]<nums[mid+1])
+                l = mid+1;
+        }
         return -1;
     }
 };
