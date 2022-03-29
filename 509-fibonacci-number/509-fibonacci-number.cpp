300ad11fda4ea1a6e1fe7621850725899fa216bc
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-03-29 22:30:26 +0530

    Time: 22 ms (12.26%), Space: 6 MB (41.15%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,11 +3,6 @@ public:
     int fib(int n) {
         if(n==0 || n==1)
             return n;
-        int memo[n+1];
-        memo[0] = 0;
-        memo[1] = 1;
-        for(int i=2; i<=n; i++)
-            memo[i] = memo[i-2]+memo[i-1];
-        return memo[n];
+        return fib(n-1)+fib(n-2);
     }
 };
