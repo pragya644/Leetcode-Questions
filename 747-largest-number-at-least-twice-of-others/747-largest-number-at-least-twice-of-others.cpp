d74706707d41c5ef318799b182c65676cf7ea72a
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-04-01 00:53:11 +0530

    Time: 3 ms (74.92%), Space: 10.9 MB (26.42%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.cpp
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& arr) {
-        int maxi = INT_MIN;
-        int n = arr.size();
-        for(int i=0; i<n; i++)
-            maxi = max(maxi, arr[i]);
-        for(int i=0; i<n; i++)
+        if(arr.size()==1)
+            return 0;
+        int max1 = INT_MIN, max2 = INT_MIN;
+        int idx = 0;
+        for(int i=0; i<arr.size(); i++)
         {
-            if(maxi!=arr[i])
+            if(arr[i]>max1)
             {
-                if(maxi<2*arr[i])
-                    return -1;
+                max2 = max1;
+                max1 = arr[i];
+                idx = i;
             }
+            else if(arr[i]>max2)
+                max2 = arr[i];
         }
-        int ans = 0;
-        for(int i=0; i<n; i++)
-            if(arr[i]==maxi)
-            {
-                ans = i;
-                break;
-            }
-        return ans;
+        return max1>=max2*2 ? idx : -1;
     }
 };
