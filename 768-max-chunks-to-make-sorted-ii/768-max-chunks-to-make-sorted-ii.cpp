2e6927995812f494842a52ec4c316930509fcd01
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-03-31 23:58:31 +0530

    Time: 16 ms (48.06%), Space: 12.2 MB (82.22%) - LeetHub

--- 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.cpp
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
-        stack<pair<int,int>> s;
+        stack<int> s;
         for(int i=0; i<n; i++)
         {
-            pair<int,int> temp = make_pair(arr[i], arr[i]);
-            while(!s.empty() && s.top().first>temp.second)
-            {
-                auto x = s.top();
+            int currmax = s.empty()?arr[i] : max(s.top(), arr[i]);
+            while(!s.empty() && s.top()>arr[i])
                 s.pop();
-                temp.second = min(x.second, temp.second);
-                temp.first = max(x.first, temp.first);
-            }
-            s.push(temp);
+            s.push(currmax);
         }
         return s.size();
     }
