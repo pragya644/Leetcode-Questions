c5b7cb7a6b0e8dd1d75eba75b595b92b0059f0be
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-04-12 21:58:26 +0530

    Time: 507 ms (9.44%), Space: 75.9 MB (8.75%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,29 +1,37 @@
 class Solution {
+    
+    private:
+    struct point
+    {
+        int row;
+        int col;
+        int dis;
+    };
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         if(grid[0][0]==1)
             return -1;
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<int,int>> q;
-        q.push({0,0});
+        queue<point> q;
+        q.push({0,0,1});
         vector<vector<int>> dir = {{0,1},{1,0},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
         grid[0][0] = 1;
         while(q.empty()==false)
         {
-            auto curr = q.front();
-            int x = curr.first;
-            int y = curr.second;
+            point curr = q.front();
+            int x = curr.row;
+            int y = curr.col;
             if(x==m-1 && y==n-1)
-                return grid[x][y];
+                return curr.dis;
             for(auto d : dir)
             {
                 int r = x + d[0];
                 int c = y + d[1];
                 if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==0)
                 {
-                    q.push({r,c});
-                    grid[r][c] = grid[x][y] + 1;
+                    q.push({r,c,curr.dis+1});
+                    grid[r][c] = 1;
                 }
             }
             q.pop();
