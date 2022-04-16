35914e14822959ee46eeb5ed094b354faee99408
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-04-17 02:14:35 +0530

    Time: 12 ms (58.01%), Space: 17.2 MB (85.96%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -10,32 +10,34 @@
  * };
  */
 class Solution {
- public:
-   int widthOfBinaryTree(TreeNode* root) {
-    if(!root)
-        return 0;
-    int ans=0;
-    queue<pair<TreeNode* ,int>>q;
-    q.push({root,0});
-    while(!q.empty()){
-        int size=q.size();
-        int mini=q.front().second;
-        int first,last;
-        for(int i=0;i<size;i++){
-            int id=q.front().second-mini;
-            TreeNode* node=q.front().first;
-            q.pop();
-            if(i==0)
-                first=id;
-            if(i==size-1)
-                last=id;
-            if(node->left)
-                q.push({node->left,(long long int)id*2+1}); //id*2+1 is indices of these nodes
-            if(node->right)
-                q.push({node->right,(long long int)id*2+2});
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        queue<pair<TreeNode*,int>> q;
+        int ans = INT_MIN;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            int n = q.size();
+            int fidx = q.front().second;
+            int first,last;
+            for(int i=0; i<n; i++)
+            {
+                auto temp = q.front();
+                q.pop();
+                int idx = temp.second - fidx; // subtracting the idx of the first element of every row
+                if(i==0)
+                    first = idx;
+                if(i==n-1)
+                    last = idx;
+                if(temp.first->left!=NULL)
+                    q.push({temp.first->left, (long long int) 2*idx+1});
+                if(temp.first->right!=NULL)
+                    q.push({temp.first->right,(long long int) 2*idx+2});
+            }
+            ans = max(ans, last-first+1);
         }
-        ans=max(ans,last-first+1);
+        return ans;
     }
-    return ans;
-  }
-  };
+};
