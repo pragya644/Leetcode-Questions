705b527fd12b5c5478775c39693098f03039b8f5
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-04-19 03:03:49 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (45.40%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -14,14 +14,12 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL && q==NULL)
             return true;
-        if(p==NULL)
+        if(p==NULL || q==NULL)
             return false;
-        if(q==NULL)
-            return false;
-        if(p->val!=q->val)
-            return false;
-        if(isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+        if(p->val==q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+           {
             return true;
+           }
         return false;
     }
 };
