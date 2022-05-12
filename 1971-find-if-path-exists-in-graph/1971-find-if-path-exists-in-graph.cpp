d4d5b4dc3f33715089aab785e4e645d6ffec01da
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-05-13 01:29:18 +0530

    Time: 797 ms (62.72%), Space: 150.9 MB (74.85%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    bool solve(int i, int d, vector<vector<int>> &v, vector<int> &vis)
-    {
-        if(i==d)
-            return true;
-        if(vis[i]==false)
-            return false;
-        vis[i] = false;
-        for(auto x: v[i])
-            if(solve(x,d,v,vis))
-                return true;
-        return false;
-    }
-    
-bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<vector<int>> v(n);
         for(int i=0; i<edges.size(); i++)
         {
@@ -21,6 +8,23 @@ bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
             v[edges[i][1]].push_back(edges[i][0]);
         }
         vector<int> vis(n, true);
-        return solve(source,destination,v,vis);
+        queue<int> q;
+        q.push(source);
+        while(q.empty()==false)
+        {
+            int curr = q.front();
+            q.pop();
+            if(curr == destination)
+                return true;
+            vis[curr] = false;
+            for(auto x: v[curr])
+            {
+                if(vis[x])
+                {
+                    q.push(x);
+                }
+            }
+        }
+        return false;
     }
 };
