70dee48030bbabc45532252fc76f211d75a5605e
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-05-13 00:12:57 +0530

    Time: 398 ms (5.77%), Space: 118.6 MB (5.08%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        if(n==1)
-            return 1;
-        if(trust.size()==0)
-            return -1;
-        vector<int> out(n+1,0);
-        vector<int> in(n+1,0);
-        for(auto x: trust)
+    int check(int i, vector<vector<int>> &v, int n)
+    {
+        for(int j=1; j<=n; j++)
         {
-            out[x[0]]++;
-            in[x[1]]++;
+            if(j==i)
+                continue;
+            if(v[j][i]==0)
+                return -1;
         }
-        int x = -1;
+        return i;
+    }
+    
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<vector<int>> v(n+1,vector<int>(n+1));
+        for(int i=1; i<=n; i++)
+            v[i][i] = 0;
+        for(int i=0; i<trust.size(); i++)
+        {
+            int x = trust[i][0];
+            int y = trust[i][1];
+            v[x][y] = 1;
+        }
         for(int i=1; i<=n; i++)
         {
-            if(out[i]==0 && in[i]==n-1)
-                return i;
+            int count = 0;
+            for(int j=1; j<=n; j++)
+            {
+                if(v[i][j]==0)
+                    count++;
+            }
+            if(count==n)
+            {
+                int x = check(i,v,n);
+                if(x!=-1)
+                    return x;
+            }
         }
-        return x;
+        return -1;
     }
 };
