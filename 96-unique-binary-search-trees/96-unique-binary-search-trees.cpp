88bfa0aee7c1299ded7abd0a1825800cc73e62f2
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-05-16 00:45:24 +0530

    Time: 6 ms (5.46%), Space: 6.1 MB (37.17%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n==0 || n==1)
+            return 1;
+        vector<int> v(n+1,0);
+        v[0]= v[1] = 1;
+        for(int i=2; i<=n; i++)
+        {
+            for(int j=1; j<=i ; j++)
+                v[i] += v[j-1]*v[i-j];
+        }
+        return v[n];
+    }
+};
