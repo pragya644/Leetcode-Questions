2594acd0a57eff39a276665c6f413d14d51bfa5f
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-05-16 09:26:43 +0530

    Time: 74 ms (74.43%), Space: 19.3 MB (83.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,40 +1,32 @@
 class Solution {
-    
-    private:
-    struct point
-    {
-        int row;
-        int col;
-        int dis;
-    };
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         if(grid[0][0]==1)
             return -1;
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<point> q;
-        q.push({0,0,1});
-        vector<vector<int>> dir = {{0,1},{1,0},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<pair<int,int>> dir{{0,-1},{-1,0},{0,1},{1,0},{-1,-1},{1,1},{-1,1},{1,-1}};
+        queue<pair<int,int>> q;
+        q.push({0,0});
         grid[0][0] = 1;
         while(q.empty()==false)
         {
-            point curr = q.front();
-            int x = curr.row;
-            int y = curr.col;
-            if(x==m-1 && y==n-1)
-                return curr.dis;
-            for(auto d : dir)
+            auto curr = q.front();
+            q.pop();
+            int x = curr.first;
+            int y = curr.second;
+            if(x==n-1 && y==m-1)
+                return grid[x][y];
+            for(auto d: dir)
             {
-                int r = x + d[0];
-                int c = y + d[1];
-                if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==0)
+                int r = x + d.first;
+                int c = y + d.second;
+                if(r>=0 && c>=0 && r<n && c<m && grid[r][c]==0)
                 {
-                    q.push({r,c,curr.dis+1});
-                    grid[r][c] = 1;
+                    q.push({r,c});
+                    grid[r][c] = grid[x][y]+1;
                 }
             }
-            q.pop();
         }
         return -1;
     }
