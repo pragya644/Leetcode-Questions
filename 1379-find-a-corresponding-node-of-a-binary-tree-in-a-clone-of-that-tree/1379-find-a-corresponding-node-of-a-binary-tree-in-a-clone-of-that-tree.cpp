f22c47e358d41fa0d7b27a5a159894a18c845605
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-05-17 20:31:27 +0530

    Time: 730 ms (48.58%), Space: 165.9 MB (5.16%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -10,20 +10,30 @@
 
 class Solution {
 public:
-    void solve(TreeNode*root1, TreeNode*root2,TreeNode* target,TreeNode* &ans )
-    {
-      if(root1==NULL)
-          return;
-      if(root1==target){
-         ans = root2;
-         return;
-      }
-      solve(root1->left, root2->left, target,ans);
-      solve(root1->right, root2->right, target,ans);
-    }
-    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        TreeNode*ans=NULL;
-        solve(original,cloned,target,ans);
-        return ans;
+    TreeNode* getTargetCopy(TreeNode* o, TreeNode* c, TreeNode* target) {
+        if(o==target)
+            return c;
+        queue<TreeNode*> q1;
+        queue< TreeNode*> q2;
+        q1.push(o);
+        q2.push(c);
+        while(q1.empty()==false)
+        {
+            auto temp1 = q1.front();
+            q1.pop();
+            auto temp2 = q2.front();
+            q2.pop();
+            if(temp1 == target)
+                return temp2;
+            if(temp1->left!=NULL)
+                q1.push(temp1->left);
+            if(temp1->right!=NULL)
+                q1.push(temp1->right);
+            if(temp2->left!=NULL)
+                q2.push(temp2->left);
+            if(temp2->right!=NULL)
+                q2.push(temp2->right);
+        }
+        return o;
     }
 };
