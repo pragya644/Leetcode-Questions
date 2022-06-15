2a17c2b299c4421cb9b7a04b5bb83938c1d299d2
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-16 00:44:37 +0530

    Time: 20 ms (20.17%), Space: 13.8 MB (91.44%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& i, int sr, int sc, int color) {
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        vector<vector<int>>vis(i.size(),vector<int>(i[0].size(),0));
-        vector<pair<int,int>> d = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-        int m = i.size();
-        int n = i[0].size();
-        while(q.empty()==false)
-        {
-            auto curr = q.front();
-            q.pop();
-            int x = curr.first;
-            int y = curr.second;
-            int initial_color = i[x][y];
-            i[x][y] = color;
-            vis[x][y] = 1;
-            for(int k=0; k<4; k++)
-            {
-                int r = x + d[k].first;
-                int c = y + d[k].second;
-                if(r>=0 && r<m && c>=0 && c<n && i[r][c] == initial_color && vis[r][c]==0)
-                    q.push({r,c});
-            }
-        }
-        return i;
+    void solve(vector<vector<int>>& img, int i, int j, int ini_c, int c)
+    {
+        if(i<0 || i>=img.size() || j<0 || j>=img[0].size() || img[i][j]!=ini_c)
+            return;
+        img[i][j] = c;
+        solve(img,i-1,j,ini_c,c);
+        solve(img,i+1,j,ini_c,c);
+        solve(img,i,j-1,ini_c,c);
+        solve(img,i,j+1,ini_c,c);
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) {
+        int m = img.size();
+        int n = img[0].size();
+        if(img[sr][sc]!=color)
+            solve(img,sr,sc,img[sr][sc], color);
+        return img;
     }
 };
