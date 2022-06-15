732bca9f0bd34ebcd442c81aa4f9baa7fae07700
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-15 12:25:16 +0530

    Time: 7 ms (90.05%), Space: 10.2 MB (21.73%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-    int solve(int l, int r, vector<int> &nums)
-    {
-        int p = nums[r];
-        int i=l-1;
-        for(int k=l; k<r; k++)
-        {
-            if(nums[k]<p)
-            {
-                i++;
-                swap(nums[i], nums[k]);
-            }
-        }
-        swap(nums[i+1], nums[r]);
-        return i+1;
-    }
-    
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
-        k = n-k;
-        int ans = 0;
-        int l = 0, r = n-1;
-        while(true)
+        priority_queue<int, vector<int> , greater<int>> pq;
+        for(int i=0; i<k; i++)
+            pq.push(nums[i]);
+        for(int i=k; i<n; i++)
         {
-            int i = solve(l,r,nums);
-            if(i==k)
+            if(pq.top()<nums[i])
             {
-                ans = nums[i];
-                break;
+                pq.pop();
+                pq.push(nums[i]);
             }
-            else if(i<k)
-                l = i+1;
-            else
-                r = i-1;
         }
-        return ans;
+        return pq.top();
     }
 };
