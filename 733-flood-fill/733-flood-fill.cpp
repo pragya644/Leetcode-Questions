c7a07437cd962abd7d967a547c407068c09c28a9
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-16 00:36:26 +0530

    Time: 24 ms (9.22%), Space: 14.6 MB (8.23%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    void solve(int i, int j, vector<vector<int>> &img, int c0, int c1)
-    {
-        if(i<0 || j<0 || i>=img.size() || j>=img[0].size() || img[i][j]!=c0)
-            return;
-        img[i][j] = c1;
-        solve(i+1,j,img,c0,c1);
-        solve(i,j+1,img,c0,c1);
-        solve(i,j-1,img,c0,c1);
-        solve(i-1,j,img,c0,c1);
-    }
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int newColor) {
-        if(img[sr][sc]!=newColor)
-            solve(sr,sc,img,img[sr][sc],newColor);
-        return img;
+    vector<vector<int>> floodFill(vector<vector<int>>& i, int sr, int sc, int color) {
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        vector<vector<int>>vis(i.size(),vector<int>(i[0].size(),0));
+        vector<pair<int,int>> d = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        int m = i.size();
+        int n = i[0].size();
+        while(q.empty()==false)
+        {
+            auto curr = q.front();
+            q.pop();
+            int x = curr.first;
+            int y = curr.second;
+            int initial_color = i[x][y];
+            i[x][y] = color;
+            vis[x][y] = 1;
+            for(int k=0; k<4; k++)
+            {
+                int r = x + d[k].first;
+                int c = y + d[k].second;
+                if(r>=0 && r<m && c>=0 && c<n && i[r][c] == initial_color && vis[r][c]==0)
+                    q.push({r,c});
+            }
+        }
+        return i;
     }
 };
