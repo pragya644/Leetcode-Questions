c56e4c4a6c9ddb106bd9475a99413ae6a2f7dc7a
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-19 22:23:21 +0530

    Time: 64 ms (79.66%), Space: 26.4 MB (81.60%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    
-    bool check(string s1, string s2)
-    {
-        for(int i=0; i<s2.length(); i++)
-        {
-            if(s1[i]!=s2[i])
-                return false;
-        }
-        return true;
-    }
-    
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         sort(products.begin(), products.end());
         vector<vector<string>> ans;
         int n = products.size();
+        int l = 0, r = n-1;
         for(int i=0; i<searchWord.length(); i++)
         {
             vector<string> temp;
-            string prefix = searchWord.substr(0,i+1);
-            int k=0;
-            while(temp.size()<3 && k<n)
+            while(l<=r &&(products[l].length()<=i || products[l][i]!=searchWord[i]))
             {
-                if(check(products[k], prefix))
-                {
-                    temp.push_back(products[k]);
-                }
-                k++;
+                l++;
+            }
+            while(r>=l &&(products[r].length()<=i || products[r][i]!=searchWord[i]))
+            {
+                r--;
+            }
+            int len = r-l+1;
+            for(int j=0; j<min(3,len); j++)
+            {
+                temp.push_back(products[l+j]);
             }
             ans.push_back(temp);
         }
