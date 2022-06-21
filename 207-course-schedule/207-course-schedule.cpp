011e4cd91dbce4140ce3af2e8d7745ca3ccb7cef
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-22 00:43:41 +0530

    Time: 32 ms (50.80%), Space: 13.3 MB (88.86%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<int>> &adj, vector<int> &vis, vector<bool> &temp, int i) {
-        if(temp[i])
-            return false;
-        if(vis[i]==1)
-            return true;
-        vis[i] = 1;
-        temp[i] = true;
-        for(auto x: adj[i]) {
-            if(dfs(adj,vis,temp,x)==false)
-                return false;
-        }
-        temp[i] = false;
-        return true;
-    }
+    //time complexity is O(n+e), e is no of prerequisite or pre.size();
+    //space compexity is 
     
+    //tropological sorting
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<vector<int>> adj(n);
         for(int i=0; i<pre.size(); i++) {
             adj[pre[i][0]].push_back(pre[i][1]);
         }
-        vector<bool> temp(n,false);
-        vector<int> vis(n,0);
-        for(int i=0; i<n; i++)
-        {
-            if(vis[i]==0)
+        vector<int> deg(n,0);
+        for(int i=0; i<n; i++) {
+            for(auto x: adj[i]) {
+                deg[x]++;
+            }
+        }
+        for(int i=0; i<n; i++) {
+            int node = 0;
+            while(node<n)
             {
-                if(dfs(adj,vis,temp,i)==false)
-                    return false;
+                if(deg[node]==0)
+                    break;
+                node++;
+            }
+            if(node==n) {
+                return false;
+            }
+            deg[node]--;
+            for(auto x: adj[node]) {
+                deg[x]--;
             }
         }
         return true;
     }
-    
-    
-    
 };
