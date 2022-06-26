8ee1038f9c1d2d8c13712fbd781ab6669f5c5bf3
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-26 12:10:11 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (24.58%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         if(n==1)
             return nums[0];
-        vector<int> dp(n+1);
+        vector<int> dp(n);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
         for(int i=2; i<n; i++)
