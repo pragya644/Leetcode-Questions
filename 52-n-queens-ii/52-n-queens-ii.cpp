2d36f4d6b997e3b126089d96e8ad6f9048fa4d57
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-29 12:36:54 +0530

    Time: 10 ms (40.07%), Space: 6.2 MB (57.25%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    
+    bool can_place(int r, int c, vector<vector<int>> &v)
+    {
+        int x = r;
+        int y = c;
+        x--;
+        y--;
+        while(x>=0 && y>=0)
+        {
+            if(v[x][y]==1)
+                return false;
+            x--;
+            y--;
+        }
+        x = r;
+        y = c;
+        x--;
+        while(x>=0)
+        {
+            if(v[x][y]==1)
+                return false;
+            x--;
+        }
+        x = r;
+        x--;
+        y++;
+        while(x>=0 && y<v.size())
+        {
+            if(v[x][y]==1)
+                return false;
+            x--;
+            y++;
+        }
+        return true;
+    }
+    
+    void is_valid(int row, int col,int n, vector<vector<int>> &v, int &ans)
+    {
+        if(row==n)
+        {
+            ans++;
+        }
+        if(!can_place(row,col,v))
+        {
+            return;
+        }
+        v[row][col] = 1;
+        for(int col=0; col<n; col++)
+        {
+            is_valid(row+1,col,n,v,ans);
+        }
+        v[row][col] = 0;
+    }
+    
+    int totalNQueens(int n) {
+        vector<vector<int>> v(n, vector<int> (n,0));
+        int ans = 0;
+        for(int i=0; i<n; i++)
+        { 
+            is_valid(0,i,n,v,ans);
+        }
+        return ans/n;
+    }
+};
