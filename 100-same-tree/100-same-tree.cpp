89593c680b765c2001e868ac23d166de27021b4f
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-30 13:15:33 +0530

    Time: 7 ms (20.11%), Space: 10.1 MB (6.81%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -16,10 +16,8 @@ public:
             return true;
         if(p==NULL || q==NULL)
             return false;
-        if(p->val==q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
-           {
-            return true;
-           }
-        return false;
+        if(p->val!=q->val)
+            return false;
+        return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
     }
 };
