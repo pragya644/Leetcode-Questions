70208c9e61804cf0e378f24ad8387f6a5282ba4d
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-06-30 23:13:49 +0530

    Time: 52 ms (17.76%), Space: 28.9 MB (19.71%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -12,57 +12,22 @@
 class Solution {
 public:
     
-    bool is_present(TreeNode* root, TreeNode* subRoot)
+    bool isSame(TreeNode* r, TreeNode* s)
     {
-        queue<TreeNode*> q1;
-        queue<TreeNode*> q2;
-        q1.push(root);
-        q2.push(subRoot);
-        while(!q1.empty() && !q2.empty())
-        {
-            int n1 = q1.size();
-            int n2 = q2.size();
-            if(n1!=n2)
-                return false;
-            auto curr1 = q1.front();
-            auto curr2 = q2.front();
-            q1.pop();
-            q2.pop();
-            if(curr1->val!=curr2->val)
-                return false;
-            if(curr1->left!=NULL && curr2->left!=NULL){ 
-                q1.push(curr1->left);
-                q2.push(curr2->left);
-            }
-            else if(curr1->left!=NULL || curr2->left!=NULL)
-                return false;
-            if(curr1->right!=NULL && curr2->right!=NULL){
-                q1.push(curr1->right);
-                q2.push(curr2->right);
-            }
-            else if(curr1->right!=NULL || curr2->right!=NULL)
-                return false;
-        }
-        if(q1.empty()==false || q2.empty()==false)
+        if(!r && !s)
+            return true;
+        if(!r || !s)
             return false;
-        return true;
+        if(r->val!=s->val)
+            return false;
+        return isSame(r->left,s->left) && isSame(r->right, s->right);
     }
     
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        queue<TreeNode*> q;
-        q.push(root);
-        while(q.empty()==false)
-        {
-            auto curr = q.front();
-            q.pop();
-            if(curr->val == subRoot->val)
-            {
-                if(is_present(curr,subRoot))
-                    return true;
-            }
-            if(curr->left!=NULL) q.push(curr->left);
-            if(curr->right!=NULL) q.push(curr->right);
-        }
-        return false;
+        if(root==NULL)
+            return false;
+        if(isSame(root,subRoot))
+            return true;
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
     }
 };
