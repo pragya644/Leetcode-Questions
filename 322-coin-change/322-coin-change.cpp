5500e22b2432aa071291b1ae21c6cfc0efc7b7e5
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-02 15:53:28 +0530

    Time: 182 ms (25.87%), Space: 46.2 MB (16.90%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,48 +1,31 @@
 class Solution {
 public:
     
-    // int solve(int i, vector<int> &coins, int &amount,int &ans){
-    //     if(i>=coins.size())
-    //         return INT_MAX;
-    //     if(amount==0)
-    //         return ;
-    //     if(coins[i]<=amount)
-    //     {
-    //         ans = ans +  min(1+solve(i,coins,amount-coins[i],ans), solve(i+1,coins, amount, ans));
-    //     }
-    //     else{
-    //         ans += solve(i+1,coins,amount,ans);
-    //     }
-    // }
+    //Recursion : time complexity is O(2^n) where is n is no of coins 
+    
+    int solve(int i, vector<int> &coins, int amount,int &ans, vector<vector<int>> &dp){
+        if(i>=coins.size())
+            return INT_MAX-1;
+        if(amount==0)
+            return dp[i][amount]=0;
+        if(dp[i][amount]!=-1)
+            return dp[i][amount];
+        if(coins[i]<=amount)
+        {
+            ans = min(1+solve(i,coins,amount-coins[i],ans,dp), solve(i+1,coins, amount, ans,dp));
+        }
+        else{
+            ans = solve(i+1,coins,amount,ans,dp);
+        }
+        return dp[i][amount]=ans;
+    }
     
     int coinChange(vector<int>& coins, int amount) {
-        // return solve(0,coins,amount);
-        
+        int ans = 0;
         int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
-        for(int i=0; i<=amount; i++){
-            dp[0][i] = INT_MAX-1;
-        }
-        for(int i=0; i<=n; i++){
-            dp[i][0] = 0;
-        }
-        for(int i=1; i<=amount; i++){
-            if(i%coins[0]==0){
-                dp[1][i] = i/coins[0];
-            }
-            else{
-                dp[1][i] = INT_MAX-1;
-            }
-        }
-        for(int i=2; i<=n; i++){
-            for(int j=1; j<=amount; j++){
-                if(coins[i-1]<=j){
-                    dp[i][j] = min(dp[i][j-coins[i-1]]+1, dp[i-1][j]);
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][amount]>amount?-1:dp[n][amount];
-    }
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int res =  solve(0,coins,amount,ans,dp);
+        return res>amount?-1:res;
+    }  
+        
 };
