4fafe645bd2b2ef58e415a4625f7fdfcfaef44f8
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-02 19:30:58 +0530

    Time: 82 ms (71.75%), Space: 10.4 MB (38.60%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     
+    //Time complexity is O(n^2*n) = O(n^3);
+    //space complexity is O(n^2);
+    
     int solve(int i, int j, vector<int> &cuts, vector<vector<int>> &dp){
         if(i>j)
             return 0;
@@ -16,11 +19,25 @@ public:
     
     int minCost(int n, vector<int>& cuts) {
         sort(cuts.begin(), cuts.end());
+        int size = cuts.size();
         cuts.insert(cuts.begin(),0);
         cuts.push_back(n);
-        int size = cuts.size();
-        vector<vector<int>> dp(size+1,vector<int> (size+1,-1));
-        int i=1, j=size-2;
-        return solve(i,j,cuts,dp);
+        vector<vector<int>> dp(size+2,vector<int> (size+2,0));
+        for(int i=size; i>0; i--)
+        {
+            for(int j=0; j<=size; j++)
+            {
+                if(i>j)
+                    continue;
+                int cost = INT_MAX;
+                for(int k=i; k<=j; k++)
+                {
+                    int mini = cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j];
+                    cost = min(cost, mini);
+                }
+                dp[i][j] = cost;
+            }
+        }
+        return dp[1][size];
     }
 };
