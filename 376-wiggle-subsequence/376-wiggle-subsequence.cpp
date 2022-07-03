98caf165a4093bfbd779357726983504da8c5a95
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-03 11:20:45 +0530

    Time: 7 ms (39.62%), Space: 7.1 MB (43.94%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        if(nums.size()==1)
-            return 1;
-        vector<int> diffBtwSucc;
-        for(int i=1; i<nums.size(); i++)
-        {
-            int diff = nums[i]-nums[i-1];
-            diffBtwSucc.push_back(diff);
-        }
-        int n = diffBtwSucc.size();
-        vector<int> maxLen(n,1);
-        int ans = 0;
+        int n = nums.size();
+        if(n==0)
+            return 0;
+        int pos = 1, neg = 1;
         for(int i=1; i<n; i++)
         {
-            if(diffBtwSucc[i]==0)
-                continue;
-            for(int j=0; j<i; j++)
-            {
-                if(diffBtwSucc[i]>0 && diffBtwSucc[j]<0)
-                {
-                    maxLen[i] = max(maxLen[i], maxLen[j]+1);
-                }
-                else if(diffBtwSucc[i]<0 && diffBtwSucc[j]>0)
-                {
-                    maxLen[i] = max(maxLen[i], maxLen[j]+1);
-                }
-            }
-            ans = max(ans, maxLen[i]);
+            if(nums[i]>nums[i-1])
+                pos = neg+1;
+            else if(nums[i]<nums[i-1])
+                neg = pos+1;
         }
-        return ans+1;
+        return max(pos,neg);
     }
 };
