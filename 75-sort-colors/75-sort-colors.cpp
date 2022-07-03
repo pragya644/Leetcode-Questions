aa35bd5601e1ffab75fa0b2cc29fa33a928580d9
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-03 23:47:16 +0530

    Time: 6 ms (23.07%), Space: 8.3 MB (23.63%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
+    
+    // // 2 0 2 1 1 0
+    //    left = -1  right = n  mid = 0;
+    //    right--
+    // // 0 0 2 1  1 2 , right = n-1;
+    // // if mid
+    
+    // time complexity is O(n)
+    
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int low = 0, mid = 0;
-        int high = n-1;
-        while(mid<=high)
+        int size = nums.size();
+        int left = -1;
+        int right = size;
+        int mid = 0;
+        while(mid<right)
         {
             if(nums[mid]==0)
             {
-                swap(nums[mid],nums[low]);
-                low++;
+                left++;
+                swap(nums[left], nums[mid]);
                 mid++;
             }
-            else if(nums[mid]==1)
-                mid++;
             else if(nums[mid]==2)
             {
-                swap(nums[mid],nums[high]);
-                high--;
+                right--;
+                swap(nums[mid], nums[right]);
+            }
+            else
+            {
+                mid++;
             }
         }
     }
