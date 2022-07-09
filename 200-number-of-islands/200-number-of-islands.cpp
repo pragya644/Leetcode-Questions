4f31cd4b419d73cb8b5d23bc82b43173df83448f
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-10 02:05:31 +0530

    Time: 40 ms (84.57%), Space: 12.1 MB (99.88%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    void solve(vector<vector<char>> &grid, int i, int j)
+    
+    void markWater(int i, int j, vector<vector<char>> &grid)
     {
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]!='1')
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]!='1')
             return;
         grid[i][j] = '2';
-        solve(grid, i-1,j);
-        solve(grid, i+1,j);
-        solve(grid, i,j-1);
-        solve(grid, i,j+1);
+        markWater(i-1,j,grid);
+        markWater(i+1,j,grid);
+        markWater(i,j-1,grid);
+        markWater(i,j+1,grid);
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
         int n = grid.size();
         int m = grid[0].size();
+        int ans = 0;
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
                 if(grid[i][j]=='1')
                 {
-                    solve(grid,i,j);
                     ans++;
+                    markWater(i,j,grid);
                 }
             }
         }
