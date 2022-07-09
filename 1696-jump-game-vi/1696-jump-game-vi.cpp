4e1f7c8ec514e469f8d45e78a9a46fdb8ad94c7e
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-09 13:51:49 +0530

    Time: 329 ms (45.57%), Space: 81.9 MB (56.82%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
+    
+    // time complexity of multiset is O(nlogK), because n for traversal and find function in mutliset take O(logk) where k is the size of the multiset
+    
+    //time complexity of monostanic queue or dequeu is O(n)
+    
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> dp(n,INT_MIN);
+        vector<int> dp(n);
+        deque<int> q;
         dp[0] = nums[0];
-        multiset<int, greater<int>> m; //decreasing order
-        m.insert(dp[0]);
+        q.push_back(0);
         for(int i=1; i<n; i++)
         {
-            if(m.size()>k)
-            {
-                auto it = m.find(dp[i-k-1]);
-                m.erase(it);
-            }
-            auto it = m.begin();
-            dp[i] = max(dp[i], nums[i]+*it);
-            m.insert(dp[i]);
+            if(i-q.front()>k)
+                q.pop_front();
+            dp[i] = nums[i] + dp[q.front()];
+            while(q.size()>0 && dp[q.back()]<dp[i])
+                q.pop_back();
+            q.push_back(i);
         }
         return dp[n-1];
     }
