1f841922490da528e1b01699f6990b28649ae840
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-10 16:06:29 +0530

    Added solution - LeetHub

---
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -0,0 +1,76 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        vector<int> dis(V, INT_MAX);
+        dis[S] = 0;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({dis[S],S});
+        while(pq.empty()==false)
+        {
+            auto curr = pq.top();
+            pq.pop();
+            int currNode = curr.second;
+            int currDis = curr.first;
+            for(auto x: adj[currNode])
+            {
+                int next = x[0];
+                int nextDis = x[1];
+                if(dis[next]>dis[currNode]+nextDis)
+                {
+                    dis[next] = dis[currNode]+nextDis;
+                    pq.push({dis[next],next});
+                }
+            }
+        }
+        return dis;
+    }
+};
+
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
