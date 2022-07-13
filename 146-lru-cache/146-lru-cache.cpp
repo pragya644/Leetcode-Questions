3230548cc4ec587989ba2fec7c5c7a53a80283e1
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-13 17:00:56 +0530

    Time: 550 ms (73.79%), Space: 175.9 MB (28.44%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,41 +1,45 @@
 class LRUCache {
 public:
     
-    list<pair<int,int>> l;
-    unordered_map<int,list<pair<int,int>>::iterator> m;
+    unordered_map<int,int> m; // key value pair store;
+    list<int> l;  //recent used key into linkedi
+    unordered_map<int,list<int>::iterator> add; //storing address of key
     int cap;
     
     LRUCache(int capacity) {
-        cap = capacity;
+       cap = capacity; 
     }
     
     int get(int key) {
-        if(m.find(key)!=m.end())
-        {
-            l.splice(l.begin(),l,m[key]);
-            auto temp = m[key];
-            return temp->second;
-        }
-        return -1;
+        if(m.find(key)==m.end())
+            return -1;
+        list<int>::iterator curr = add[key];
+        l.erase(curr);
+        l.push_front(key);
+        add[key] = l.begin(); //bcoz now key list address is in begining of list
+        return m[key];
     }
     
     void put(int key, int value) {
-        if(m.find(key)!=m.end())
+        if(m.find(key)!=m.end()) //if key is already present than change the value
         {
-            l.splice(l.begin(), l, m[key]);
-            auto temp = m[key];
-            temp->second = value;
+            m[key] = value; 
+            list<int>::iterator curr = add[key]; //key position
+            l.erase(curr);  
+            l.push_front(key);
+            add[key] = l.begin();
             return;
         }
         if(m.size()==cap)
         {
-            auto temp = l.back();
-            auto val = temp.first;
+            int key = l.back();
             l.pop_back();
-            m.erase(val);
+            m.erase(key);
+            add.erase(key);
         }
-        l.push_front({key,value});
-        m[key] = l.begin();
+        m[key] = value;
+        l.push_front(key);
+        add[key] = l.begin();
     }
 };
 
