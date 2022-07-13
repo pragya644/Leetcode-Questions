6b13b042157947dca3d754479f4093c16819a40c
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-13 21:26:32 +0530

    Time: 2 ms (83.94%), Space: 7.5 MB (89.62%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    void solve(vector<vector<int>> &ans, vector<int> &temp, int i, vector<int> &nums)
+    
+    void solve(int i,vector<int>&temp,vector<vector<int>> &ans,vector<int>&nums)
     {
-        if(i>nums.size())
-        {
-            return;
-        }
         ans.push_back(temp);
+        if(i==nums.size())
+            return;
         for(int j=i; j<nums.size(); j++)
         {
             if(j!=i && nums[j]==nums[j-1])
                 continue;
             temp.push_back(nums[j]);
-            solve(ans, temp, j+1, nums);
+            solve(j+1,temp,ans,nums);
             temp.pop_back();
         }
     }
@@ -21,7 +20,7 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         vector<int> temp;
-        solve(ans, temp, 0, nums);
+        solve(0,temp,ans,nums);
         return ans;
     }
 };
