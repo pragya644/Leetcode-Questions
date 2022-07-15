a65bd7f7a95c34c8350e0494af7f7768d9f1989f
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-15 22:53:10 +0530

    Time: 95 ms (65.67%), Space: 8.3 MB (96.20%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     
-    //Time complexity is O(n^2*n) = O(n^3);
-    //space complexity is O(n^2);
+    int dp[101][101];
     
-    int solve(int i, int j, vector<int> &cuts, vector<vector<int>> &dp){
-        if(i>j)
+    int findCost(int rodStart, int rodEnd, vector<int>&cuts, int cutStart,int cutEnd)
+    {
+        if(cutStart>cutEnd)
             return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+        if(dp[cutStart][cutEnd]!=-1)
+            return dp[cutStart][cutEnd];
         int cost = INT_MAX;
-        for(int k=i; k<=j; k++){
-            int mini = cuts[j+1]-cuts[i-1] + solve(i,k-1,cuts,dp) + solve(k+1,j,cuts,dp);
-            cost = min(cost,mini);
+        for(int i=cutStart; i<=cutEnd; i++)
+        {
+          cost = min(cost, (rodEnd-rodStart) + findCost(rodStart,cuts[i],cuts,cutStart,i-1) + findCost(cuts[i],rodEnd,cuts,i+1,cutEnd));
         }
-        return dp[i][j] = cost;
+        return dp[cutStart][cutEnd]=cost;
     }
     
     int minCost(int n, vector<int>& cuts) {
-        sort(cuts.begin(), cuts.end());
-        int size = cuts.size();
-        cuts.insert(cuts.begin(),0);
-        cuts.push_back(n);
-        vector<vector<int>> dp(size+2,vector<int> (size+2,0));
-        for(int i=size; i>0; i--)
-        {
-            for(int j=0; j<=size; j++)
-            {
-                if(i>j)
-                    continue;
-                int cost = INT_MAX;
-                for(int k=i; k<=j; k++)
-                {
-                    int mini = cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j];
-                    cost = min(cost, mini);
-                }
-                dp[i][j] = cost;
-            }
-        }
-        return dp[1][size];
+        memset(dp,-1,sizeof(dp));
+        sort(cuts.begin(),cuts.end());
+        int noOfCuts = cuts.size();
+        return findCost(0,n,cuts,0,noOfCuts-1);
     }
 };
