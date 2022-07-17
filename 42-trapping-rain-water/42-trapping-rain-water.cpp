71c89552573656d56bcd075f331a34b1466746aa
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-17 23:20:39 +0530

    Time: 25 ms (49.09%), Space: 19.8 MB (75.82%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        if(n<3)
+        int totalWaterTrap = 0;
+        int noOfBar = height.size();
+        if(noOfBar<3)
             return 0;
         int left = 0;
-        int right = n-1;
-        int ans = 0;
-        int leftmax = height[0], rightmax = height[n-1];
+        int right = noOfBar-1;
+        int leftMax = height[left] ;
+        int rightMax = height[right];
+        
         while(left<=right)
         {
-            // if(height[left]<=height[right])
-            if(leftmax<=rightmax)
-            {
-                if(height[left]<leftmax)
-                    ans = ans + (leftmax-height[left]);
-                else
-                    leftmax = max(leftmax, height[left]);
+            if(leftMax<=rightMax){
+                if(height[left]<leftMax){
+                    totalWaterTrap = totalWaterTrap + (leftMax-height[left]);
+                } else {
+                    leftMax = max(leftMax,height[left]);
+                }
                 left++;
-            }
-            else
-            {
-                if(height[right]<rightmax)
-                    ans = ans + (rightmax-height[right]);
-                else
-                    rightmax= max(rightmax, height[right]);
+            } else{
+                if(height[right]<rightMax){
+                    totalWaterTrap = totalWaterTrap + (rightMax-height[right]);
+                } else{
+                    rightMax = height[right];
+                }
                 right--;
             }
         }
-        return ans;
+        return totalWaterTrap;
     }
 };
