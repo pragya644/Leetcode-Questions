255d1a5ed193577a19b4761bb66225f168827ed8
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-19 18:00:35 +0530

    Time: 3 ms (42.42%), Space: 6.9 MB (18.84%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,31 +1,68 @@
 class MyStack {
 public:
-    queue<int> q;
+    queue<int> q1;
+    queue<int> q2;
+    int curr;
+    
     MyStack() {
         
     }
     
     void push(int x) {
-        q.push(x);
-        for(int i=0; i<q.size()-1; i++)
-        {
-            q.push(q.front());
-            q.pop();
+        if(q1.empty()){
+            q2.push(x);
+        }else{
+            q1.push(x);
         }
     }
     
     int pop() {
-        int x = q.front();
-        q.pop();
-        return x;
+        if(!q1.empty()){
+            int x;
+            while(q1.empty()==false){
+                x = q1.front();
+                q1.pop();
+                if(q1.empty())
+                    return x;
+                q2.push(x);
+            }
+        }else if(!q2.empty()){
+            while(q2.empty()==false){
+                int x = q2.front();
+                q2.pop();
+                if(q2.empty())
+                    return x;
+                q1.push(x);
+            }
+        }
+        return -1;
     }
     
     int top() {
-        return q.front();
+        if(!q1.empty()){
+            int x;
+            while(q1.empty()==false){
+                x = q1.front();
+                q1.pop();
+                q2.push(x);
+            }
+            return x;
+        }else if (!q2.empty()){
+            int x;
+            while(q2.empty()==false){
+                x = q2.front();
+                q2.pop();
+                q1.push(x);
+            }
+            return x;
+        }
+        return -1;
     }
     
     bool empty() {
-        return q.empty();
+        if(q1.empty() && q2.empty())
+            return true;
+        return false;
     }
 };
 
