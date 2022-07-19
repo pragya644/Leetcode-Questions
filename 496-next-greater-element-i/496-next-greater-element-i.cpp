72cdd151a1b474c8193c782eb32eb818a9584e92
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-19 20:50:36 +0530

    Time: 8 ms (69.06%), Space: 8.8 MB (70.18%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+    
+    //time complexity is O(n), n is the size of nums2
+    
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> m;
         int n = nums2.size();
-        for(int i=0; i<nums2.size(); i++){
-            m[nums2[i]] = i;
-        }
-        vector<int> ans;
+        unordered_map<int,int> m;
+        int size = nums1.size();
         for(int i=0; i<nums1.size(); i++){
-            int idx = m[nums1[i]];
-            int num = -1;
-            for(int j=idx+1; j<n; j++){
-                if(nums2[j]>nums1[i]){
-                    num = nums2[j];
-                    break;
-                }
+            m[nums1[i]] = i;
+        }
+        stack<int> s;
+        vector<int> ans(size,-1);
+        for(int i=0; i<n; i++){
+            while(!s.empty() && s.top()<nums2[i]){
+                ans[m[s.top()]] = nums2[i];
+                s.pop();
             }
-            ans.push_back(num);
+            if(m.find(nums2[i])!=m.end()){
+                s.push(nums2[i]);
+            }
         }
         return ans;
     }
