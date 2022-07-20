6c8569d0d649b57eb075828f5f9cd896fe844027
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-20 22:39:56 +0530

    Time: 535 ms (77.15%), Space: 176 MB (25.46%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,41 +1,44 @@
 class LRUCache {
 public:
     
-    unordered_map<int,int> m; // key value pair store;
-    list<int> l;  //recent used key into linkedi
-    unordered_map<int,list<int>::iterator> add; //storing address of key
+    //we can not use remove because it take O(n) time;
+    
+    list<int> l;
+    unordered_map<int,int> m;
+    unordered_map<int,list<int>::iterator> add;
     int cap;
     
     LRUCache(int capacity) {
-       cap = capacity; 
+        cap = capacity;
     }
     
     int get(int key) {
         if(m.find(key)==m.end())
             return -1;
-        list<int>::iterator curr = add[key];
-        l.erase(curr);
+        list<int>::iterator it = add[key];
+        l.erase(it);
         l.push_front(key);
-        add[key] = l.begin(); //bcoz now key list address is in begining of list
+        add[key] = l.begin();
         return m[key];
     }
     
     void put(int key, int value) {
-        if(m.find(key)!=m.end()) //if key is already present than change the value
-        {
-            m[key] = value; 
-            list<int>::iterator curr = add[key]; //key position
-            l.erase(curr);  
+        if(m.find(key)!=m.end()){
+            
+            m[key] = value;
+            list<int>::iterator it = add[key];
+            l.erase(it);
             l.push_front(key);
             add[key] = l.begin();
             return;
+            
         }
-        if(m.size()==cap)
-        {
-            int key = l.back();
-            l.pop_back();
-            m.erase(key);
-            add.erase(key);
+        if(m.size()==cap){
+                int least = l.back();
+                l.pop_back();
+                m.erase(least);
+                add.erase(key);
+            
         }
         m[key] = value;
         l.push_front(key);
