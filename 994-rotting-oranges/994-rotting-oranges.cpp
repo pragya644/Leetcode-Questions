94a3168ab1ead3eda89efd6d8b262b34124ccc4c
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-28 14:21:32 +0530

    Time: 18 ms (22.32%), Space: 13.2 MB (47.28%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -4,19 +4,20 @@ public:
         int row = grid.size();
         int col = grid[0].size();
         
-        vector<vector<bool>> vis(row, vector<bool>(col,false));
+        int noOfFreshOranges = 0;
         
         queue<pair<int,int>> rottenOranges;
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 if(grid[i][j]==2){
-                    vis[i][j] = true;
                     rottenOranges.push({i,j});
                 }
+                if(grid[i][j]==1){
+                    noOfFreshOranges++;
+                }
             }
+            
         }
-        
-        
         
         vector<pair<int,int>> dir = {{-1,0},{0,-1},{1,0},{0,1}};
         int time = -1;
@@ -29,14 +30,13 @@ public:
                 rottenOranges.pop();
                 int x = curr.first;
                 int y = curr.second;
-                grid[x][y] = 2;
-                
                 for(auto it: dir){
                     int r = x+it.first;
                     int c = y+it.second;
-                    if(r>=0 && c>=0 && r<row && c<col && grid[r][c]==1 && !vis[r][c]){
+                    if(r>=0 && c>=0 && r<row && c<col && grid[r][c]==1){
                         rottenOranges.push({r,c});
-                        vis[r][c] = true;
+                        grid[r][c] = 2;
+                        noOfFreshOranges--;
                     }
                 }
             }  
@@ -44,13 +44,8 @@ public:
         if(time == -1)
             time++;
         
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(grid[i][j]==1)
-                    return -1;
-            }
-        }
-        
+        if(noOfFreshOranges>0)
+            return -1;
         return time;
         
     }
