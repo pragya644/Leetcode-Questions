8f205f9d965b24886d37e1ed72899ae338b06624
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-07-28 11:45:47 +0530

    Time: 30 ms (7.78%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,14 +3,13 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())
             return false;
-        unordered_map<char,int> m;
-        for(int i=0; i<s.length(); i++)
-            m[s[i]]++;
-        for(int i=0; i<t.length(); i++)
-            m[t[i]]--;
-        for(auto x:m)
-            if(x.second!=0)
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            if(s[i]!=t[i])
                 return false;
+        }
         return true;
     }
 };
