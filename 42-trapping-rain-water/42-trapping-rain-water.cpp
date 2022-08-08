b446d04abf09e3d673f97ba617767bc675567b7e
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-08-08 23:33:17 +0530

    Time: 33 ms (24.64%), Space: 20.7 MB (11.43%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int totalWaterTrap = 0;
-        int noOfBar = height.size();
-        if(noOfBar<3)
-            return 0;
-        int left = 0;
-        int right = noOfBar-1;
-        int leftMax = height[left] ;
-        int rightMax = height[right];
-        
-        while(left<=right)
+    int trap(vector<int>& bridges) {
+        int noOfBridges = bridges.size();
+    vector<int> leftMax(noOfBridges, -1);
+    vector<int> rightMax(noOfBridges, -1);
+
+    for (int i = 1; i < noOfBridges; i++)
+    {
+        if (leftMax[i - 1] > bridges[i])
         {
-            if(leftMax<=rightMax){
-                if(height[left]<leftMax){
-                    totalWaterTrap = totalWaterTrap + (leftMax-height[left]);
-                } else {
-                    leftMax = max(leftMax,height[left]);
-                }
-                left++;
-            } else{
-                if(height[right]<rightMax){
-                    totalWaterTrap = totalWaterTrap + (rightMax-height[right]);
-                } else{
-                    rightMax = height[right];
-                }
-                right--;
-            }
+            leftMax[i] = leftMax[i - 1];
         }
-        return totalWaterTrap;
+        else if (bridges[i - 1] > bridges[i])
+        {
+            leftMax[i] = bridges[i - 1];
+        }
+    }
+
+    for (int i = noOfBridges - 2; i >= 0; i--)
+    {
+        if (rightMax[i + 1] > bridges[i])
+        {
+            rightMax[i] = rightMax[i + 1];
+        }
+        else if (bridges[i + 1] > bridges[i])
+        {
+            rightMax[i] = bridges[i + 1];
+        }
+    }
+
+    int maxWater = 0;
+    for (int i = 1; i < noOfBridges - 1; i++)
+    {
+        if (leftMax[i] != -1 && rightMax[i] != -1)
+        {
+            int minOfMaxBridges = min(leftMax[i], rightMax[i]);
+            int waterStore = minOfMaxBridges - bridges[i];
+            maxWater += waterStore;
+        }
+    }
+    return maxWater;
     }
 };
