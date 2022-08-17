b3d8186081da7d16ba5d3bb2c6d309c10d283f30
Pragya Singhal <71804175+pragya644@users.noreply.github.com>
2022-08-17 12:13:14 +0530

    Time: 25 ms (83.19%), Space: 14 MB (50.04%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     
-    // dfs cycle checking
+    // troplogical sort or bfs cycle checking
     //time complexity is O(V+E)
-    
-    bool isCycle(int i,vector<bool> &vis,vector<bool> &currVis, vector<vector<int>> &adj){
-        if(vis[i]==true){ //to prevent tle
-            return false;
-        }
-        vis[i] = true;
-        currVis[i] = true;
-        for(auto x: adj[i]){
-            if(currVis[x]==false && isCycle(x,vis,currVis,adj)){
-                return true;
-            }else if(currVis[x]){
-                return true;
-            }
-        }
-        currVis[i] = false;
-        return false;
-    }
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         for(auto x: prerequisites){
             adj[x[0]].push_back(x[1]);
         }
-        vector<bool> vis(numCourses,false);
-        vector<bool> currVis(numCourses,false);
+        vector<int> inDegree(numCourses, 0);
         for(int i=0; i<numCourses; i++){
-            if(vis[i]==false){
-                if(isCycle(i,vis,currVis,adj)){
-                    return false;
+            for(auto x: adj[i]){
+                inDegree[x]++;
+            }
+        } 
+        queue<int> q;
+        for(int i=0; i<numCourses; i++){
+            if(inDegree[i]==0){
+                q.push(i);
+            }
+        }
+        int count = 0;
+        vector<bool> vis(numCourses,false);
+        while(q.empty()==false){
+            auto curr = q.front();
+            q.pop();
+            vis[curr] = true;
+            count++;
+            for(auto x: adj[curr]){
+                if(vis[x]==true)
+                    continue;
+                inDegree[x]--;
+                if(inDegree[x]==0){
+                    q.push(x);
                 }
             }
         }
-        return true;
+        if(count == numCourses)
+            return true;
+        return false;
     }
 };
